0786da625737dcf8f55b8035965c007cf1c04d5f
santiagosant <santidevargas@gmail.com>
2020-09-04 17:24:04 -0300

    1er commit

---
+++ Assets/Scenes/Controlador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour
+{
+
+    public void CambiarEscena(string nombre)
+    {
+        print("Cambiar escena a " + nombre);
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void Salir()
+    {
+        print("saliendo");
+        Application.Quit();
+    }
+
+}
